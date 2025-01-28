58107361986243b005faa2a9fa795902fe718d24
guerreiro05 <viniarsenios8@gmail.com>
2025-01-27 21:15:27 -0300

    Primeiro comit

---
+++ app/page.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Enviando para o github</h1>
+      <p>atividade finalizada!</p>
+
+
+
+    </div>
+  );
+}
